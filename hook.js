06b6f8cdaf75d2690418b114477e6cfc3c86c3a6
Paco <34928425+pacocoursey@users.noreply.github.com>
2021-07-02 15:19:01 +0000

    Fix refreshing on pages with a hash in URL (#10)
    * Update hook.js
    
    * Use a more unique query param name

--- hook.js
+++ hook.js
@@ -1,5 +1,5 @@
 const { useEffect } = require('react')
-const { useRouter } = require('next/router')
+const { useRouter, NextRouter } = require('next/router')
 const getConfig = require('next/config').default
 
 // private API, might break on the next version
@@ -21,14 +21,32 @@ module.exports.useRemoteRefresh = function ({
         if (event.data === '\uD83D\uDC93') {
           return
         }
-        
+
         if (shouldRefresh(JSON.parse(event.data).path)) {
-          router.replace(router.asPath, router.asPath, {
-            scroll: false,
-          })
+          // Change a query parameter on every refresh to forcefully refresh the props
+          // otherwise, pages with a hash like `/blog#my-heading` will not refresh
+          replace(router, '1').then(() => replace(router, null))
         }
       })
       return () => source.close()
     }, [router])
   }
+}
+
+const QUERY_NAME = '__remote-refresh__'
+
+async function replace(router: NextRouter, refresh: string | null) {
+  const query = { ...router.query }
+  if (!refresh) delete query[QUERY_NAME]
+  else query[QUERY_NAME] = refresh
+
+  return router.replace(
+    {
+      pathname: router.pathname,
+      hash: window.location.hash,
+      query
+    },
+    undefined,
+    { scroll: false, shallow: false },
+  )
 }
