ab509d5922bef915615ada068ffd7e78e4deba6d
Arnav Gosain <arnav@arnavgosain.com>
2021-07-30 16:52:13 +0530

    Add type declarations

---
+++ types.d.ts
@@ -0,0 +1,22 @@
+import { NextConfig } from "next/dist/next-server/server/config-shared";
+
+declare module "next-remote-refresh" {
+  interface Options {
+    paths: string[];
+    ignored: string;
+  }
+
+  type Config = NextConfig | {};
+
+  type Plugin = (options: Options) => (nextConfig: Config) => Config;
+
+  const plugin: Plugin;
+
+  export default plugin;
+}
+
+declare module "next-remote-refresh/hook" {
+  export function useRemoteRefresh(options?: {
+    shouldRefresh?: (path: string) => boolean;
+  }): void;
+}
