eb7d9239e71f838deb40cfa4bce7501d2f742984
Travis Arnold <ftntravis@gmail.com>
2021-12-06 23:24:17 -0800

    better port variable (#19)

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const createServer = require('./server')
 
-let port
-
 module.exports = function plugin(options) {
+  let port
+
   return function withConfig(nextConfig = {}) {
     if (process.env.NODE_ENV !== 'production') {
       if (!port) {
         port = createServer(options)
       }
 
-      nextConfig.publicRuntimeConfig = {
-        ...nextConfig.publicRuntimeConfig,
-        __remoteRefreshPort: port,
+      if (nextConfig.env === undefined) {
+        nextConfig.env = {}
       }
+
+      nextConfig.env.remoteRefreshPort = port
     }
 
     return nextConfig
